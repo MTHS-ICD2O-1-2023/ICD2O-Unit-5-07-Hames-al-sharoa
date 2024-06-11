08c5d0b8fb0623e1ec4cf3f612149d4960401cb0
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-06-11 18:27:53 +0000

    save al changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) HAMES AL SHAROA All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: hames al sharoa
+// Created on: may 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-07-JS/sw.js", {
+    scope: "/ICS2O-Unit5-07-JS/",
+  })
+}
+
+/**
+ * This function counts up from 1 to the user's chosen value.
+ */
+function myButtonClicked() {
+  const num = parseInt(document.getElementById("integer").value)
+  let sum = 0
+
+  document.getElementById("formula").innerHTML =
+    "Sn = " + num + "/2(" + num + "+1)"
+
+  for (let counter = 1; counter < num + 1; counter++) {
+    sum += counter
+  }
+
+  document.getElementById("sum").innerHTML = "Answer = " + sum
+}
